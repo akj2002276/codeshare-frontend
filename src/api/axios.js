c2178985b9c76ff310a0425cf7242baf242b7671
Avinash Jha <avinashjha@Avinashs-MacBook-Air-2.local>
2026-05-25 14:57:35 +0530

    communtiy section added in backend and frontend

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,15 +1,15 @@
-// import axios from "axios";
-
-// const API = axios.create({
-//   baseURL: "https://codeshare-backend-t6dh.onrender.com/api",
-// });
-
-// export default API;
-
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://codeshare-backend-t6dh.onrender.com/api",
 });
 
 export default API;
+
+// import axios from "axios";
+
+// const API = axios.create({
+//   baseURL: "http://localhost:8000/api",
+// });
+
+// export default API;
